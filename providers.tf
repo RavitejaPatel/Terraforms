98ef98306ba428b327870882ab06a5ddde71f995
RavitejaPatel <raviteja.idams134@gmail.com>
2026-05-20 05:38:24 -0700

    moved rbac

--- providers.tf
+++ providers.tf
@@ -9,15 +9,15 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    use_azure_rbac       = true
+    resource_group_name  = "rg-terraform-backend"
+    storage_account_name = "ravitejatfstatebackend"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  use_oidc       = true
+  use_azure_rbac = true # FIXED: Moved from the backend block to the provider block
 }
