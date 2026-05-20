caee50ff9953ced666e9c42d246446106ebe70ea
RavitejaPatel <raviteja.idams134@gmail.com>
2026-05-20 06:43:16 -0700

    add native go.mod and fix terratest import paths

---
+++ test/go.mod
@@ -0,0 +1,8 @@
+module test
+
+go 1.21
+
+require (
+	github.com/gruntwork-io/terratest v0.48.1
+	github.com/stretchr/testify v1.9.0
+)
--- test/infra_test.go
+++ test/infra_test.go
@@ -1,26 +1,26 @@
 package test
 
 import (
-"testing"
+	"testing"
 
-"://github.com"
-"://github.com"
-"://github.com"
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEnterprisePipelineDeployment(t *testing.T) {
-t.Parallel()
+	t.Parallel()
 
-terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-TerraformDir: "../",
-})
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
 
-defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
-terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
-rgName := terraform.Output(t, terraformOptions, "resource_group_name")
+	rgName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-exists := azure.ResourceGroupExists(t, rgName, "")
-assert.True(t, exists, "The target enterprise resource group was not provisioned successfully.")
+	exists := azure.ResourceGroupExists(t, rgName, "")
+	assert.True(t, exists, "The target enterprise resource group was not provisioned successfully.")
 }
